a03936c41d7a68522feeba923bdb07234f054b9b
Mikhail Petrov <79767549+MikaelPetrov@users.noreply.github.com>
2021-08-07 23:12:25 +0300

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,8 +4,8 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      // target: "http://localhost:3100/",
-      target: "http://80.78.245.74:3100/",
+      target: "http://localhost:3100/",
+//    target: "http://80.78.245.74:3100/",
       pathRewrite: {
         "^/api/": "/", // remove base path
       },
