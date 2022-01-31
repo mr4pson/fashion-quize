702c0647f6810df43c3cc302ec4c9f7b6e0b5ef6
Evgenii Goriachkin <Evgenii.Goriachkin@payever.org>
2022-01-31 03:42:54 +0300

    redesigned tasks page

--- src/components/pages/UserPage/TasksPage/TaskCard/TaskCard.tsx
+++ src/components/pages/UserPage/TasksPage/TaskCard/TaskCard.tsx
@@ -1,4 +1,3 @@
-import { Button } from "antd";
 import classNames from "classnames";
 import { TypeTask } from "components/pages/StylistPage/TasksPage/types";
 import { memo } from "react";
@@ -14,36 +13,37 @@ const TaskCard: React.FC<Props> = ({ task, onTaskCancel }) => {
     <div className={styles["task-card"]}>
       <div className={styles["task-card__header"]}>
         <div className={styles["task-card__info"]}>
-          <div className={styles["task-card__number"]}>{task.id}</div>
+          <div className={styles["task-card__number"]}>{task.id}.</div>
           <div className={styles["task-card__type"]}>{task.type.title}</div>
-        </div>
-        <div
-          className={classNames(
-            styles["task-card__status"],
-            styles["task-card__status--on-way"]
-          )}
-        >
-          {task.status.title}
         </div>
       </div>
       <div className={styles["task-card__body"]}>
         <div className={styles["task-feature"]}>
-          <div className={styles["task-feature__title"]}>Дата создания</div>
+          <div className={styles["task-feature__title"]}>Дата создания:</div>
           <div className={styles["task-feature__value"]}>{task.createdAt}</div>
         </div>
         <div className={styles["task-feature"]}>
-          <div className={styles["task-feature__title"]}>Исполнить до</div>
+          <div className={styles["task-feature__title"]}>Исполнить до:</div>
           <div className={styles["task-feature__value"]}>
             {task.date} {task.time}
           </div>
         </div>
-        <Button
-          onClick={() => onTaskCancel(task.id)}
-          className={styles["task-card__cancel-btn"]}
-          type={"link"}
-        >
-          Отменить
-        </Button>
+        <div className={styles["task-card__footer"]}>
+          <div
+            className={classNames(
+              styles["task-card__status"],
+              styles["task-card__status--on-way"]
+            )}
+          >
+            {task.status.title}
+          </div>
+          <button
+            onClick={() => onTaskCancel(task.id)}
+            className={styles["task-card__cancel-btn"]}
+          >
+            Отменить
+          </button>
+        </div>
       </div>
     </div>
   );
